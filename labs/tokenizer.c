6b84a4cc28bed8dd051b58d33fe00bab71f3e535
aazambrano2 <aazambrano2@miners.utep.edu>
2021-09-14 13:35:29 -0600

    updated skeleton functions

--- labs/tokenizer.c
+++ labs/tokenizer.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdboo.h>
+#include <stdbool.h>
 # define MAXLINE 1000
 
-int getinput(char line[], int maxLine);
+
 char** tokenize(char* str);
-char* substring(char*start, int current, int previous);
-char *word_start(char str);
+char *word_start(char* str);
 char *end_word(char* str);
 int count_token(char*str);
 char *copy_str(char *intStr, short len);
@@ -16,46 +15,25 @@ int main(){
     char line[MAXLINE]; /* current input line */
     printf("Please enter the input string:\n");
     printf("> ");
-    
-    while((len = getinput(line,MAXLINE))>0)
-	//get one input string
-	break;
-    printf("Tokenizing %s\n",line);
+    //get user input
+    fgets(line,MAXLINE,stdin);
+    printf("USER INPUT: %s",line);
     tokenize(line);
     return 0;
 }
 
-
-int getinput(char s[], int maxLine){
-    int c, i;
-    for(i = 0; (c=getchar()) != EOF && c!= '\n'; i++){
-	if (i < maxLine)
-	    s[i] = c;
-    }
-    if( c == '\n'){
-        s[i] = c;
-        ++i;
-    }
-    if(i >= maxLine)
-        s[999] = '\0';
-    else
-        s[i] = '\0';
-    return i;
-}
-
-
-
 //TODO: Fix tokenizer problem (tok not being Hi in Hi There)
 //substituted space with _ for testing purposes
+/**
 char** tokenize(char* str){
     char* start;
     char* tok;
     char ** token;
 
-    /*
-    printf("%c\n",*str);
-    printf("%c\n",**token);
-    */
+    
+    //printf("%c\n",*str);
+    //printf("%c\n",**token);
+    
     int i,previous;
     previous = 0;
     start = str;
@@ -76,36 +54,60 @@ char** tokenize(char* str){
     printf("Current position of tok: %s\n",tok);
     return token;
 }
+**/
 
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').
    Zero terminators are not printable (therefore false) */
 bool delim_character(char c){
-
+    
+    return c == ' ' || c == '\t';
 }
 
 /* Return true (non-zero) if c is a non-whitespace
    character (not tab or space).
    Zero terminators are not printable (therefore false) */
 bool non_delim_character(char c){
-
+    return c != ' ' || c != '\t';
 }
+
+
 
 /* Returns a pointer to the first character of the next
    space-separated word*/
 char *word_start(char* str){
-
+    char* s = str;
+    while(non_delim_character(*s)){
+	if(delim_character(*s))
+	    break;
+        ++s;
+    }
+    return s + 1;
 }
-
 /* Returns a pointer to the first space character of the zero
 terminated string*/
 char *end_word(char* str){
-
+    char* s = str;
+    while(non_delim_character(*s)){
+	if(delim_character(*s))
+	    break;
+        ++s;
+    }
+    return s;
 }
+
 // counts the number of words or tokens
 int count_tokens(char* str){
-
-
+    char c;
+    int acc;
+    while(*str != '\0'){
+        if(delim_character(*str))
+	  ++acc;
+        ++str;
+    }
+    if (*str == '\0')
+       ++acc;
+    return acc;
 }
 /* Returns a freshly allocated zero-terminated vector of freshly allocated
    space-separated tokens from zero-terminated str.
@@ -114,17 +116,67 @@ int count_tokens(char* str){
      tokens[1] = "world"
      tokens[2] = "string"
      tokens[3] = 0
+inStr is start i believe
 */
 char *copy_str(char *inStr, short len){
-
+    char* new_string = (char*) malloc(len*sizeof(char));
+    char* s = new_string;
+    while((*new_string = *inStr) != ' '){
+        ++inStr;
+        ++new_string;
+    }
+    *new_string ='\0';
+    //Go back to the intial character of the tokenized string
+    new_string = s;
+    return new_string;
 }
 
 char** tokenize(char* str){
+    char** dummy;
+    char* start;
+    char* end;
+    char c;
+    char* current_string;
+    int t_count,sub_length;
+    t_count = count_tokens(str);
+    //Initialize token array here?
+    //initial
+    start = str;
+    while(*str != '\0'){
+        //update end
+        if(delim_character(*str)){
+		end = end_word(start);
+		sub_length = (end - start) + 1;
+		//printf("%s",start);
+		printf("Current Sub String length: %d\n",sub_length);
+	        //store this to a token
+		current_string = copy_str(start,sub_length);
+		printf("CURRENT SUB STRING: %s\n",current_string);
+	        //update start
+	        start = str+1;
+	        //next
+	        str = start;
+	}
+        ++str;
+    }
+    //last set of characters
+   
+    if(*str == '\0'){
+	end = str;
+        printf("%s\n",end);
+	sub_length = end - start;
+        //store
+	current_string = copy_str(start,sub_length);
+    }
+    printf("Current Sub String length:%d\n",sub_length);
+    printf("CURRENT SUB STRING:%s\n",current_string);
+    return dummy;
 }
 
 
 void print_all_tokens(char** tokens){
 }
+
 
 /*
 char* substring(char* start, int current,int previous){
